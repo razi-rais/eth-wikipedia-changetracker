238ef241c25030c9569ef42bcf28db6c17c47795
Viktor Dikov <viktordd@gmail.com>
2018-02-10 01:22:04 -0500

    Added user and id links.

--- WikiChangeAngular/src/app/articles/article/article.component.ts
+++ WikiChangeAngular/src/app/articles/article/article.component.ts
@@ -5,6 +5,7 @@ import { MatTableDataSource, MatSort } from '@angular/material';
 import * as moment from 'moment';
 import * as _ from 'underscore';
 
+import { CONFIG } from '../../core';
 import { Article } from '../shared/article.model';
 import { ArticleWeb3Service } from '../shared/article-web3.service';
 
@@ -52,6 +53,34 @@ export class ArticleComponent implements OnInit, OnDestroy {
           .value();
         this.dataSource.data = this.articles;
       }, error => { }));
+  }
+
+  getUserUrl(article: Article) {
+    if (!article || !article.user) {
+      return '';
+    }
+    const isIP = article.user.match(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/);
+    return isIP
+      ? this.getUrl(CONFIG.baseUrls.wikiContributions, article)
+      : this.getUrl(CONFIG.baseUrls.wikiUser, article);
+  }
+
+  getRevisionOldUrl(article: Article) {
+    if (!article) {
+      return '';
+    }
+    return this.getUrl(CONFIG.baseUrls.wikiRevisionOld, article);
+  }
+
+  getRevisionNewUrl(article: Article) {
+    if (!article) {
+      return '';
+    }
+    return this.getUrl(CONFIG.baseUrls.wikiRevisionNew, article);
+  }
+
+  getUrl(url: string, obj: any) {
+    return url.replace(/\{(\w+)\}/g, (match, group) => obj.hasOwnProperty(group) ? obj[group] : '');
   }
 
 }
