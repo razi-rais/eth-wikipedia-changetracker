d699a16968833e36f442a1aee724598635cfe736
Razi Rais <razibinrais@outlook.com>
2018-02-19 13:31:13 -0500

    Added check for empty password

--- WikiBlockApi/WikiBlockApi.py
+++ WikiBlockApi/WikiBlockApi.py
@@ -13,7 +13,10 @@ CORS(app)
 web3_url= sys.argv[1]
 account = sys.argv[2]
 contract_address = sys.argv[3]
-password = sys.argv[4]
+password = ""
+if not sys.argv[4]:
+   password = sys.argv[4]
+
 
 abi = json.loads('[{"constant":false,"inputs":[{"name":"id","type":"uint256"},{"name":"request_id","type":"string"},{"name":"title","type":"string"},{"name":"url","type":"string"},{"name":"revision_old","type":"uint256"},{"name":"revision_new","type":"uint256"},{"name":"timestamp","type":"uint256"},{"name":"change_type","type":"string"},{"name":"user","type":"string"},{"name":"comment","type":"string"}],"name":"UpdateArticleHistory","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"","type":"address"},{"indexed":false,"name":"id","type":"uint256"},{"indexed":false,"name":"title","type":"string"},{"indexed":false,"name":"url","type":"string"},{"indexed":false,"name":"revision_old","type":"uint256"},{"indexed":false,"name":"revision_new","type":"uint256"},{"indexed":false,"name":"timestamp","type":"uint256"},{"indexed":false,"name":"change_type","type":"string"},{"indexed":false,"name":"user","type":"string"},{"indexed":false,"name":"comment","type":"string"}],"name":"articleUpdateEvent","type":"event"}]')
 gas = 100000
@@ -34,7 +37,10 @@ def update_article():
 
     data = json.loads(request.json)
     web3 = Web3(HTTPProvider(web3_url))
-    web3.personal.unlockAccount(account,password)
+    
+    if  len(password) > 0:
+        web3.personal.unlockAccount(account,password)
+    
     wiki_contract = web3.eth.contract(contract_address,abi=abi)
     tx_receipt = ""
     try:
